09a4f0178d9bdbe4f697866cfdfe548dda31c31c
JJHAVOK <156384546+JJHAVOK@users.noreply.github.com>
2024-07-29 12:32:49 -0400

    Restarted - New NAV BAR Begin

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
-(function($) {
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
 
-	"use strict";
-
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			$this.find('.dropdown-menu').removeClass('show');
-	});
-
-})(jQuery);
+});
